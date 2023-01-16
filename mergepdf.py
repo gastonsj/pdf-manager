215db430a9473157d468243dfb5561ba6c506fa6
Gaston Leiva <gaston.leiva@bghtechpartner.com>
2023-01-16 11:15:48 -0300

    Add output folder. Create if not exist

--- mergepdf.py
+++ mergepdf.py
@@ -2,11 +2,19 @@ from PyPDF2 import PdfMerger
 import os
 
 pdfs = [archivo for archivo in os.listdir('./') if archivo.endswith(".pdf")]
-nombre_archivo_salida = "output.pdf"
+foldername="output_folder"
+output_filename = "output.pdf"
+fullpath=foldername + "//" + output_filename
+print(fullpath)
+isExist = os.path.exists(foldername)
+if not isExist:
+   # Create a new directory because it does not exist
+   os.makedirs(foldername)
+
 merger = PdfMerger()
 
 for pdf in pdfs:
     merger.append(open(pdf, 'rb'))
 
-with open(nombre_archivo_salida, 'wb') as salida:
+with open(fullpath, 'wb') as salida:
     merger.write(salida)
